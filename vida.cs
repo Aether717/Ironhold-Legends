dfb7cc540339f3a083be8f481e4e2d32f359ae59
Joel Cuadras <jcuadrasalvarez@gmail.com>
2025-05-21 10:01:47 +0200

    Create vida.cs
    Script del item de vida

---
+++ vida.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class vida : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            bool vidaRecuperada = GameManager.Instance.RecuperarVida();
+            if (vidaRecuperada)
+            {
+                Destroy(this.gameObject);
+            }
+
+            
+        }
+    }
+}
